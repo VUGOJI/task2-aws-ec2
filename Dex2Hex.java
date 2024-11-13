91f6d94535f15118148919ef80f73fca962ea07d
Vincent Ugoji <VUgoji300@caledonian.ac.uk>
2024-11-13 16:50:08 +0000

    udpate

--- Dex2Hex.java
+++ Dex2Hex.java
@@ -18,6 +18,15 @@ private static final Logger logger = Logger.getLogger(Dec2Hex.class.getName());
             return;
         }
 
+
+	        try {
+            // Try to parse the argument to an integer
+            Arg1 = Integer.parseInt(args[0]);
+        } catch (NumberFormatException e) {
+            logger.log(Level.SEVERE, "Error: The input provided is not a valid integer.",);
+            return;
+        }
+
         Arg1 = Integer.parseInt(args[0]);
         char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
         int rem, num;
