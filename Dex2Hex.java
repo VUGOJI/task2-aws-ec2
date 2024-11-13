dc482ecae8f630eea800b21d35c181617ff52b85
Vincent Ugoji <VUgoji300@caledonian.ac.uk>
2024-11-13 17:24:50 +0000

    seperated vaiable declaration

--- Dex2Hex.java
+++ Dex2Hex.java
@@ -21,7 +21,8 @@ class Dec2Hex {
         }
 
         char ch[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
-        int rem, num = arg1;
+        int rem;
+        int  num = arg1;
         StringBuilder hexadecimal = new StringBuilder();
 
         logger.log(Level.INFO, "Converting the Decimal Value {0} to Hex...", num);
