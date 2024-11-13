169c81f5b0c6faa7764a5ed3fa4971047f0eab8c
Vincent Ugoji <VUgoji300@caledonian.ac.uk>
2024-11-13 17:13:17 +0000

    added string builder to replace traditional string

--- Dex2Hex.java
+++ Dex2Hex.java
@@ -22,18 +22,18 @@ class Dec2Hex {
 
         char ch[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
         int rem, num = Arg1;
-        String hexadecimal = "";
+        StringBuilder hexadecimal = new StringBuilder();
 
         logger.log(Level.INFO, "Converting the Decimal Value {0} to Hex...", num);
 
-        // Convert decimal to hexadecimal
+        // Convert decimal to hexadecimal using StringBuilder
         while (num != 0) {
             rem = num % 16;
-            hexadecimal = ch[rem] + hexadecimal;
+            hexadecimal.insert(0, ch[rem]);  // Insert character at the beginning
             num = num / 16;
         }
 
-        logger.log(Level.INFO, "Hexadecimal representation is: {0}", hexadecimal);
+        logger.log(Level.INFO, "Hexadecimal representation is: {0}", hexadecimal.toString());
     }
 }
 
